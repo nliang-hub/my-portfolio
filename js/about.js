0b103d8bce3aa492964041e9773fdd6ba014bc07
Leelee Liang <leeliang@publicisgroupe.net>
2025-10-02 19:15:35 -0700

    added grid layout

--- js/about.js
+++ js/about.js
@@ -1,3 +1,10 @@
 document.getElementById('skills-btn').onclick = () => {
     document.getElementById('skills-list').classList.remove('hidden');
 }
+
+const colors = ['red', 'yellow', 'green', 'blue'];
+var index = 0;
+
+document.querySelectorAll('.grid-item').forEach((el, index) => {
+    el.style.backgroundColor = colors[index % colors.length];
+})
